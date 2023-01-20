137a85d947a15b0124e66ef62481b474d037cf59
nsc1337 <nikitosskarjevskij@gmail.com>
2023-01-20 13:25:59 +0100

    parallax-wallpaper-cards
    nscOld/parallax-wallpaper-cards

---
+++ ready-html/js/app.js
@@ -0,0 +1,12 @@
+new Swiper('.slider', {
+	// direction: 'vertical', вертикальний скролл. але треба адаптація фоток.
+	speed: 2100,
+	parallax: true,
+	spaceBetween: 20,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.4
+	}
+});
+
+
